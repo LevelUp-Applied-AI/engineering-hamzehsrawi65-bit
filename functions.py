30f4067961f335dba4fecfe9de224bb96e62b8c1
Hamzeh Awad <hamzehsrawi65@gmail.com>
2026-03-02 12:11:19 +0300

    fix: make all 5 tests pass

--- functions.py
+++ functions.py
@@ -6,14 +6,14 @@ def is_even(n):
     return n % 2 == 0
 
 
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9 / 5) + 32
+def celsius_to_fahrenheit(c):
+    return (c * 9 / 5) + 32
 
 
-def count_vowels(text):
+def count_vowels(s):
     vowels = "aeiou"
     count = 0
-    for ch in text.lower():
+    for ch in s.lower():
         if ch in vowels:
             count += 1
     return count
